8e5a12cd1e3cd5b21b3799127c368e2a413180d2
abdoujtk <abdouenfr@gmail.com>
2026-07-10 18:55:22 +0100

    Add project files.

---
+++ DailyRegisterBlazor/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DailyRegisterBlazor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
